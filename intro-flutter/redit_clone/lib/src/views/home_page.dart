8679aa3b280c8096a0e5800ccefa7cfd61b8b563
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2025-02-26 18:59:16 -0600

    disenio de tarjeta y agregando opntap para la navegacion

--- intro-flutter/redit_clone/lib/src/views/home_page.dart
+++ intro-flutter/redit_clone/lib/src/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:redit_clone/src/api/productos.dart';
+import 'package:redit_clone/src/widgets/item_list.dart';
 import 'package:redit_clone/src/widgets/side_menu.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,19 +16,7 @@ class HomePage extends StatelessWidget {
       body: ListView.builder(
         itemCount: productos.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Column(
-              children: [
-                Image(
-                  height: 250,
-                  fit: BoxFit.fill,
-                  image: NetworkImage(productos[index]['image']),
-                ),
-                Text(productos[index]['title']),
-                Text('USD${productos[index]['price']}'),
-              ],
-            ),
-          );
+          return ItemList(producto: productos[index]);
         },
       ),
     );
